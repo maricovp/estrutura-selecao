95d7ea2c68c3ea6cd86a4fa90862d664fbd6aa6a
professor-wanderson-timoteo <wanderson_timoteo@outlook.com>
2024-03-27 16:01:39 -0400

    Add exemplo de estrutura de seleção encadeada

---
+++ selecao-encadeada.js
@@ -0,0 +1,9 @@
+let dia = "sábado";
+
+if (dia === "sábado") {
+  console.log("Parabéns você merece descansar!");
+} else if (dia === "domingo") {
+  console.log("Dia de passear com a família!");
+} else {
+  console.log("Levanta e vai estudar!")
+}
