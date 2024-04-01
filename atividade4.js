e7202bf535fefc08245e1c4611ca805c52be6fa2
maricovp <marcos2015vinicuis@gmail.com>
2024-04-01 10:37:26 -0400

    atividade4 adicionada

---
+++ atividade4.js
@@ -0,0 +1,12 @@
+/*Como você pode usar uma declaração if, else if, else para verificar 
+se um número é positivo, negativo ou zero em JavaScript?*/
+
+let negativo = -1
+let positivo = 1
+let neutro = 0
+if (negativo <-1  ) {
+    console.log ("negativo")
+}
+else {
+    console.log ("positivo")
+}
