4e2a6e17c0c9183d242499265b1796af5a277ebf
maricovp <marcos2015vinicuis@gmail.com>
2024-04-01 10:37:08 -0400

    atividade1 adicionada

---
+++ atividade1.js
@@ -0,0 +1,10 @@
+/*Como você pode usar uma declaração if para verificar se uma 
+variável é maior que 10 em JavaScript? */
+
+let marcos = 11
+
+if(marcos > 10) {
+    console.log(" parabéns")
+} else {
+    console.log("receba")
+}
