f5468ebba059a8d2a79d4652cad889501ff7fcb5
maricovp <marcos2015vinicuis@gmail.com>
2024-04-01 10:37:20 -0400

    atividade3 adicionada

---
+++ atividade3.js
@@ -0,0 +1,9 @@
+/*Como você pode usar uma declaração if para verificar se uma 
+variável é do tipo string em JavaScript? */
+var mensagem = "Olá mundo";
+
+if (typeof mensagem === 'string') {
+    console.log('A variável é uma string.');
+} else {
+    console.log('A variável não é uma string.');
+}
